9e30d7c85770b8bd9f5a44ccc3c29125d7876db4
sorochinskii <sorochinsky.alex@gmail.com>
2024-03-14 11:36:13 +0500

    Fix migrations. Badge, pre-commit erd, readme.

--- generate_erd.py
+++ generate_erd.py
@@ -4,14 +4,24 @@ import sys
 current_dir = os.getcwd()                   # noqa #isort:skip
 sys.path.append(current_dir + '/source/')   # noqa #isort:skip
 
-from eralchemy2 import render_er
+import pip
 
-from source.db.models.buildings import BaseCommon, Building
-from source.db.models.persons import Person
-from source.db.models.rooms import Room
-from source.db.models.vendors import Vendor
 
-print(BaseCommon.metadata.tables)
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+
 if __name__ == '__main__':
+    install('eralchemy2')
+    install('sqlalchemy')
+    from db.models.base import BaseCommon
+    from eralchemy2 import render_er
 
+    from source.db.models.buildings import Building
+    from source.db.models.persons import Person
+    from source.db.models.rooms import Room
+    from source.db.models.vendors import Vendor
     render_er(BaseCommon, 'erd.png')
