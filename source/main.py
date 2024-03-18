f98d6201d083af2bd2c5699cd339969670d75402
sorochinskii <sorochinsky.alex@gmail.com>
2024-03-18 16:07:29 +0500

    crud_db_v1 add.

--- source/main.py
+++ source/main.py
@@ -17,7 +17,6 @@ app.mount("/v1", app_v1)
 
 if __name__ == "__main__":
     path = os.getcwd()
-    print(os.path.abspath(os.path.join(path, os.pardir)))
     import uvicorn
     uvicorn.run("__main__:app", host="127.0.0.1", port=8845, reload=True,
                 reload_dirs=['source'])
