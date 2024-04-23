d4ba2a9376cd970a575776d94da2f7edee395960
sorochinskii <sorochinsky.alex@gmail.com>
2024-04-23 14:37:38 +0500

    testcontainers

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ def run_migrations(dsn: str) -> None:
     command.upgrade(alembic_cfg, 'head')
 
 
-@pytest.yield_fixture(scope='session')
+@pytest.fixture(scope='session')
 def event_loop(request):
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
